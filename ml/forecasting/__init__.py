e68faa7443db8385f0f6922cb68042c01dd98dfb
Parth Chandna <Chandnaparth64@gmail.com>
2026-06-06 18:01:18 +0530

    chore: export Prophet forecasting public API from package init
    Re-export ForecastingPipeline and ProphetForecaster for clean imports.

--- ml/forecasting/__init__.py
+++ ml/forecasting/__init__.py
@@ -1,1 +1,8 @@
 """Time-series forecasting models for utilization prediction."""
+
+from ml.forecasting.prophet_forecaster import (
+    ForecastingPipeline,
+    ProphetForecaster,
+)
+
+__all__ = ["ForecastingPipeline", "ProphetForecaster"]
